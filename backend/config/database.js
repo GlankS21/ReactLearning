22e1afdaf16a4e1b8ee549eedd023e88a9b217be
GlankS21 <asgar21502@gmail.com>
2025-11-24 17:42:27 +0300

    up

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,6 +13,7 @@ require('dotenv').config();
 // })
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
